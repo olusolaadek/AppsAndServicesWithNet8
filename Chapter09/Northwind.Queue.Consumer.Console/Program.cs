b503f24e10f8d0c3cc54e7b1435b73ddbf16419b
FESTUS ADEKUNLE <olusolaadek@hotmail.com>
2025-08-07 20:20:11 -0600

    Subscribe to the queue - Add a console app that receive the the queue message

---
+++ Chapter09/Northwind.Queue.Consumer.Console/Program.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Northwind.Queue.Models; // To use ProductQueueMessage.
+using RabbitMQ.Client; // To use ConnectionFactory.
+using RabbitMQ.Client.Events; // To use EventingBasicConsumer.
+using System.Text.Json; // To use JsonSerializer.
+string queueName = "product";
+ConnectionFactory factory = new() { HostName = "localhost" };
+using IConnection connection = await factory.CreateConnectionAsync();
+using IChannel channel = await connection.CreateChannelAsync();
+WriteLine("Declaring queue...");
+QueueDeclareOk response = await channel.QueueDeclareAsync(
+  queue: queueName,
+  durable: false,
+  exclusive: false,
+  autoDelete: false,
+  arguments: null);
+WriteLine($"Queue name: {response.QueueName}, Message count: {response.MessageCount}, Consumer count: {response.ConsumerCount}.");
+WriteLine("Waiting for messages...");
+AsyncEventingBasicConsumer consumer = new(channel);
+consumer.ReceivedAsync += async (model, args) =>
+{
+    byte[] body = args.Body.ToArray();
+    ProductQueueMessage? message = JsonSerializer
+      .Deserialize<ProductQueueMessage>(body);
+    if (message is not null)
+    {
+        WriteLine($"Received product. Id: {message.Product.ProductId}, Name: {message.Product.ProductName}, Message: {message.Text}");
+    }
+    else
+    {
+        WriteLine($"Received unknown: {args.Body.ToArray()}.");
+    }
+    await Task.CompletedTask;
+};
+// Start consuming as messages arrive in the queue.
+await channel.BasicConsumeAsync(queue: queueName,
+  autoAck: true,
+  consumer: consumer);
+WriteLine(">>> Press Enter to stop consuming and quit. <<<");
+ReadLine();
