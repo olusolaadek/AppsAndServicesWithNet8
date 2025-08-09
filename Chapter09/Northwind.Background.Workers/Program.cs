c52496b1ba2830e937af44823c13fbdae8430386
FESTUS ADEKUNLE <olusolaadek@hotmail.com>
2025-08-09 10:25:45 -0600

    Add the TimerWorker

--- Chapter09/Northwind.Background.Workers/Program.cs
+++ Chapter09/Northwind.Background.Workers/Program.cs
@@ -1,7 +1,16 @@
 using Northwind.Background.Workers;
 
-var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<QueueWorker>();
+namespace Northwind.Background.Workers;
 
-var host = builder.Build();
-host.Run();
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = Host.CreateApplicationBuilder(args);
+        builder.Services.AddHostedService<QueueWorker>();
+        builder.Services.AddHostedService<TimerWorker>();
+
+        var host = builder.Build();
+        host.Run();
+    }
+}
---
+++ Chapter09/Northwind.Background.Workers/TimerWorker.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.Background.Workers
+{
+    public class TimerWorker : IHostedService, IAsyncDisposable
+    {
+        private readonly ILogger<TimerWorker> _logger;
+
+
+        private int _executionCount = 0;
+        private Timer? _timer;
+        private int _seconds = 5;
+
+        public TimerWorker(ILogger<TimerWorker> logger)
+        {
+            _logger = logger;
+        }
+
+        private void DoWork(object? state)
+        {
+            int counter = Interlocked.Increment(ref _executionCount);
+            _logger.LogInformation("{0} is working. Count: {1}", nameof(TimerWorker), counter);
+        }
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("{0} is running.", nameof(TimerWorker));
+
+            _timer = new Timer(DoWork, null,TimeSpan.Zero, TimeSpan.FromSeconds(_seconds));
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("{0} is stopping.", nameof(TimerWorker));
+            _timer?.Change(Timeout.Infinite, 0);
+            return Task.CompletedTask;
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            if (_timer is IAsyncDisposable asyncTimer)
+            {
+                await asyncTimer.DisposeAsync();
+            }
+            _timer= null;
+            // _timer?.Dispose();
+            //await Task.CompletedTask;
+        }
+    }
+}
